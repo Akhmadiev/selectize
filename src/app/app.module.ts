15596f0d1d2c48d495cca21c582b4fc2b6b8ab9a
Marat Akhmadiev <v-maakhm@microsoft.com>
2018-11-19 17:22:39 +0300

    added books

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { SearchFilterPipe } from './searchFilterPipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    SearchFilterPipe
   ],
   imports: [
     BrowserModule
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { BOOKS } from '../mock-books';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+
+  books = BOOKS;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
